77a9b9345e1821fcee824503bc8e3a8c04e5d869
iyeboah22 <117760079+iyeboah22@users.noreply.github.com>
2023-09-14 12:21:09 -0400

    Delete import_data directory

--- import_data/import geopandas as pd.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib 
-import matplotlib.pyplot as plt
-import geopandas as gpd
-
-geo_mh = r'C:/Users/yeboahi/Desktop/GIS Data/import_data/Mental health Depression disorder Data.csv'
-
-df = pd.read_csv(geo_mh)
-df
